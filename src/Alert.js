7c7804337cd9a9deccaf7cf46c339e1f1e8b61de
ptthao0801 <ptthao0801@gmail.com>
2024-07-02 19:56:31 +0700

    first cm

---
+++ src/Alert.js
@@ -0,0 +1,12 @@
+﻿import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import React, { Component } from 'react';
+function Alert(props) {
+    return (
+        <div class="alert alert-warning" role="alert">
+            {props.text }
+        </div>
+    )
+}
+export default Alert;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+﻿import React, { Component } from 'react';
+import Alert from './Alert';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." />
+    )
 }
 
 export default App;
